4dd2eff50feee0586f9a401226a2041143adbd92
d22samma <d22samma@student.his.se>
2023-04-20 13:09:27 +0200

    QRight.java Collect Inputs and changed layout

--- app/src/main/java/com/example/diakoll/QRight.java
+++ app/src/main/java/com/example/diakoll/QRight.java
@@ -3,6 +3,7 @@ package com.example.diakoll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class QRight extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class QRight extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qright);
+
+        TextView ChangeDatan = findViewById(R.id.CorrectAnswer);
+        TextView ChangeDatan2 = findViewById(R.id.CorrectAnswer2);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String CAnswer = extras.getString("CAnswer");
+            ChangeDatan.setText("Du svarade Korrekt");
+            ChangeDatan2.setText("Svaret motsvarar" + CAnswer + "." );
+        }
     }
 }
