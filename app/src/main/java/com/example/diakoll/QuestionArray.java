6ca594f680f5f617c7b3c8b0eb5729c121336f3a
d22samma <d22samma@student.his.se>
2023-04-20 15:24:04 +0200

    Tillagd Question.java klass och ändrad kod inom QuestionArray.java

--- app/src/main/java/com/example/diakoll/QuestionArray.java
+++ app/src/main/java/com/example/diakoll/QuestionArray.java
@@ -1,4 +1,10 @@
 package com.example.diakoll;
 
+import java.util.ArrayList;
+
 public class QuestionArray {
+        String Questions[] = {"Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7"};
+        int rightanswer;
+        int totalanswer;
+        int currentanswer;
 }
